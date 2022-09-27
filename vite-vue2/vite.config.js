5dafd74dda90490bbde32669a5392bf864b909c8
yip <yip@augurit.com>
2022-09-27 11:06:49 +0800

    使用vite创建vue2项目

---
+++ vite-vue2/vite.config.js
@@ -0,0 +1,14 @@
+import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [createVuePlugin()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: '/src'
+      }
+    ]
+  }
+})
